cb17320bf3e5b9f1a6b499fb4f64122629efb90d
ncq <ncq@ncq-mac.lan>
2016-09-07 11:46:40 +0800

    no message

---
+++ SDK/Conf/config.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 公共SDK配置
+ */
+return array(
+    /* 应用设定 */
+    'ACTION_SUFFIX'        => 'Action',
+
+    /* 默认设定 */
+    'DEFAULT_MODULE'       => 'Index',
+    'DEFAULT_FILTER'       => 'trim,htmlspecialchars',
+
+    /* 错误设置 */
+    'ERROR_MESSAGE'        => '系统错误',
+
+    /* 日志设置 */
+    'LOG_FILE_SIZE'        => 20971520,
+
+    /* 模板引擎设置 */
+    'TMPL_ENGINE_TYPE'     => 'PHP',
+    'TMPL_TEMPLATE_SUFFIX' => '.phtml',
+
+    /* URL设置 */
+    'URL_MODEL'            => 2,
+    'URL_HTML_SUFFIX'      => '',
+
+    /* 系统变量名称设置 */
+    'CHECK_APP_DIR'        => false,
+);
