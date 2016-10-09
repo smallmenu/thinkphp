cc1e1e52287ff6782cb477f7b665dcf5cbf0d2bb
smallmenu <smallmenu@gmail.com>
2016-10-09 22:14:28 +0800

    * 更新 SDKCliController基类

--- ThinkSDK/Controller/SDKCliController.class.php
+++ ThinkSDK/Controller/SDKCliController.class.php
@@ -6,6 +6,21 @@ use Think\Controller;
 class SDKCliController extends Controller
 {
     /**
+     * @var null
+     */
+    protected $define = null;
+
+    /**
+     * @var null
+     */
+    protected $setting = null;
+
+    /**
+     * @var \Think\Cache\Driver\Redis
+     */
+    protected $cache = null;
+
+    /**
      * initialize
      */
     public function _initialize()
@@ -16,6 +31,28 @@ class SDKCliController extends Controller
         set_time_limit(0);
         error_reporting(E_ALL);
         ini_set('display_errors', 'on');
+
+        $this->define = config('define');
+        $this->setting = config('setting');
+
+        $this->_init();
+    }
+
+    /**
+     *
+     */
+    private function _init()
+    {
+        /**
+         * 初始化 cache 通过domain, prefix, module避免污染，实现应用模块私有缓存
+         */
+        $domain = $this->define['DOMAIN'];
+        if ($cache = config('cache')) {
+            $module = strtolower(MODULE_NAME);
+            $cache['prefix'] = $domain. ':'. $cache['prefix']. $module . ':';
+            config('cache.prefix', $cache['prefix']);
+            $this->cache = cache($cache);
+        }
     }
 
     /**
