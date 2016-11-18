64edcb8c603d7f48d173339a3658f7478d5ad415
smallmenu <smallmenu@gmail.com>
2016-11-18 23:45:01 +0800

    * 调整百度新闻源为json格式

--- ThinkSDK/Library/Spider/Baidu/BaiduNews.class.php
+++ ThinkSDK/Library/Spider/Baidu/BaiduNews.class.php
@@ -9,8 +9,6 @@ class BaiduNews
 
     private $keyword = null;
 
-
-
     public function __construct()
     {
 
@@ -21,8 +19,8 @@ class BaiduNews
      *
      * @param $keyword 关键词
      * @param bool $strict 严格模式，会对列表中的title进行完全匹配校验，不受分词影响，默认false
-     * @param int $pagesize  最大只能是50
-     * @param int $day  默认过滤出3天内的新闻
+     * @param int $pagesize 最大只能是50
+     * @param int $day 默认过滤出3天内的新闻
      * @return array|bool
      */
     public function getListByTitle($keyword, $strict = false, $pagesize = 50, $day = 3)
@@ -41,79 +39,52 @@ class BaiduNews
         if (empty($keyword)) {
             return false;
         } else {
-            $keyword = urlencode($keyword);
+            $keyword = urlencode('title:' . $keyword);
         }
 
-        $this->requestUrl = self::BAIDUNEWS_URL . '?tn=newstitle&rn='.$pagesize. '&word='.$keyword;
+        $this->requestUrl = self::BAIDUNEWS_URL . '?tn=json&rn=' . $pagesize . '&word=' . $keyword;
 
         $listRequest = request($this->requestUrl);
 
         if ($listRequest['httpcode'] == 200) {
             $listContent = $listRequest['content'];
+            $listContent = json_decode($listContent, true);
 
-            preg_match('#<span class="nums">找到相关新闻(.*)篇</span>#', $listContent, $baiduTotal);
+            if (isset($listContent['feed']) && $listContent['feed']['all'] > 0) {
+                $baiduTotal = intval($listContent['feed']['all']);
 
-            if (isset($baiduTotal[1])) {
-                $baiduTotal = intval(str_replace(array('约',','), '', $baiduTotal[1]));
-            }
+                $listHtmls = $listContent['feed']['entry'];
 
-            preg_match_all('#<h3 class="c-title">([\s\S]*)&\#8226;&nbsp;#U', $listContent, $listHtmls);
-
-            //print_r($listHtmls);exit;
-
-            if (isset($listHtmls[0]) && !empty($listHtmls[0])) {
-                foreach ($listHtmls[0] as $key => $listHtml) {
-
+                foreach ($listHtmls as $key => $listHtml) {
                     // URL
-                    if (preg_match('#<h3 class="c-title"><a href="(.*)"#U', $listHtml, $urls)) {
-                        $url = trim($urls[1]);
-                        $url = !empty($url) ? $url : '';
-                    }
+                    $url = trim($listHtml['url']);
+                    $url = !empty($url) ? $url : '';
 
                     // 标题
-                    if (preg_match('#<h3 class="c-title">([\s\S]*)</h3>#U', $listHtml, $titles)) {
-                        $title = trim(strip_tags($titles[1]));
-                        $title = !empty($title)? $title : '';
-                    }
+                    $title = trim(strip_tags($listHtml['title']));
+                    $title = !empty($title) ? $title : '';
 
                     // 来源
-                    if (preg_match('#<div class="c-title-author">(.*)&nbsp;&nbsp;#U', $listHtml, $sources)) {
-                        $source = trim($sources[1]);
-                        $source = !empty($source)? $source : '';
-                    }
+                    $source = trim($listHtml['source']);
+                    $source = !empty($source) ? $source : '';
 
                     // 时间
-                    if (preg_match('#&nbsp;&nbsp;(.*)&nbsp;&nbsp;#U', $listHtml, $publisheds)) {
-                        $published = trim($publisheds[1]);
-                        if (!empty($published)) {
-                            if (strpos($published, '分钟') !== false) {
-                                $published = intval(str_replace('分钟前', '', $published));
-                                $published = date('Y-m-d H:i:s', strtotime("- $published minutes"));
-                            } else if (strpos($published, '小时') !== false) {
-                                $published = intval(str_replace('小时前', '', $published));
-                                $published = date('Y-m-d H:i:s', strtotime("- $published hours"));
-                            } else {
-                                $published = str_replace(array('年', '月'), '-', $published);
-                                $published = str_replace('日', '', $published);
-                                $published = date('Y-m-d H:i:s', strtotime($published));
-                            }
-                        }
-                        $published = !empty($published)? $published : '';
-                    }
-
+                    $published = !empty($listHtml['time']) ? date('Y-m-d H:i:s') : '';
 
                     // 数据过滤
                     $cell = array(
-                        'url' => $url,
-                        'title' => $title,
-                        'source' => $source,
+                        'url'       => $url,
+                        'title'     => $title,
+                        'source'    => $source,
                         'published' => $published
                     );
+
                     if ($this->_validator($cell, $strict, $day)) {
                         $lists[] = $cell;
                     }
                 }
             }
+
         }
 
         $result['keyword'] = $keyword;
@@ -135,7 +106,7 @@ class BaiduNews
     private function _validator($data, $strict, $day)
     {
         // 校验空值
-        foreach($data as $key => $d) {
+        foreach ($data as $key => $d) {
             if (empty($d)) {
                 return false;
             }
@@ -147,7 +118,7 @@ class BaiduNews
         }
 
         // 校验日期时间，保留最近day天
-        $validatorTime = strtotime('-'. $day. ' days');
+        $validatorTime = strtotime('-' . $day . ' days');
         $publishedTime = strtotime($data['published']);
         if ($publishedTime < $validatorTime) {
             return false;
